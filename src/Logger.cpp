7bfe3103011bc93dfbaa811307740c4a98841dfd
gaslam <gaspard.lammertyn@hotmail.com>
2023-12-12 13:14:55 +0100

    Fix error saying code in unreachable

--- src/Logger.cpp
+++ src/Logger.cpp
@@ -57,30 +57,28 @@ void Logger::LogInfo(const InfoLog& info)
 void Logger::HandleLog(LogLevel type, const InfoLog& info)
 {
 #if defined(_DEBUG) || defined(DEBUG)
-	return;
-#endif
-
 	CONSOLE_SCREEN_BUFFER_INFO   csbi;
-	GetConsoleScreenBufferInfo(m_pConsoleHandle,&csbi);
+	GetConsoleScreenBufferInfo(m_pConsoleHandle, &csbi);
 	const WORD currentAttributeConsole{ csbi.wAttributes };
 
-	const auto typeInfo{ m_LogTypes[type]};
+	const auto typeInfo{ m_LogTypes[type] };
 	SYSTEMTIME st;
 	GetSystemTime(&st);
 
 	SetConsoleTextAttribute(m_pConsoleHandle, typeInfo.color);
 
 	std::wcout << "[" << typeInfo.typeName << "]: " << info.warningText << "\n\n"
-	<< "Date: " << st.wDay << "-" << st.wMonth << "-" << st.wYear << " (dd/mm/yyyy)" << "\n"
-	<< "Time: " << st.wHour << ":" << st.wMinute << ":" << st.wSecond << ":" << st.wMilliseconds << "\n"
-	<< "File: " << info.fileLocation.file_name() <<  "(" << info.fileLocation.line()  << ")" << "\n"
-	<< "Function: " << info.fileLocation.function_name() << "\n\n";
+		<< "Date: " << st.wDay << "-" << st.wMonth << "-" << st.wYear << " (dd/mm/yyyy)" << "\n"
+		<< "Time: " << st.wHour << ":" << st.wMinute << ":" << st.wSecond << ":" << st.wMilliseconds << "\n"
+		<< "File: " << info.fileLocation.file_name() << "(" << info.fileLocation.line() << ")" << "\n"
+		<< "Function: " << info.fileLocation.function_name() << "\n\n";
 
 	SetConsoleTextAttribute(m_pConsoleHandle, currentAttributeConsole);
 
-	if(type == LogLevel::ERROR)
+	if (type == LogLevel::ERROR)
 	{
 		MessageBoxW(0, info.warningText.c_str(), typeInfo.typeName.c_str(), MB_OK | MB_ICONERROR);
 		ExitGame();
 	}
+#endif
 }
