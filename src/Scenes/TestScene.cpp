9a8191f253295400dde7814af5853f4ec174edb0
gaslam <gaspard.lammertyn@hotmail.com>
2023-12-12 19:16:08 +0100

    Update TestScene to remove testcode

--- src/Scenes/TestScene.cpp
+++ src/Scenes/TestScene.cpp
@@ -2,17 +2,12 @@
 #include "TestScene.h"
 
 #include "Components/MeshRenderComponent.h"
-#include "Structs/KeyStructs.h"
 #include "Managers/InputManager.h"
 
 using namespace Engine;
 
 void TestScene::Initialize()
 {
-	const auto input{ InputManager::GetInstance() };
-	auto con{ XboxControllerButton::B };
-	auto conK{ DirectX::Keyboard::Keys::A };
-	input->BindButtonsToInput(0, con,conK,L"test");
 	GameObject* room{ AddChild(new GameObject{}) };
 	room->AddComponent<MeshRenderComponent>();
 
