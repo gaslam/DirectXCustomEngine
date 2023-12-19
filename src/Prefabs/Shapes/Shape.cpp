549dad0a8410bb4e776e8394bc161f49c15cefbc
gaslam <44768183+gaslam@users.noreply.github.com>
2023-12-19 17:13:27 +0100

    Expand shape to pass texture path

--- src/Prefabs/Shapes/Shape.cpp
+++ src/Prefabs/Shapes/Shape.cpp
@@ -3,5 +3,5 @@
 
 void Shape::Initialize(const SceneContext&)
 {
-	m_pMesh = AddComponent<MeshRenderComponent>();
+	m_pMesh = AddComponent<MeshRenderComponent>(m_TexturePath);
 }
--- src/Prefabs/Shapes/Shape.h
+++ src/Prefabs/Shapes/Shape.h
@@ -6,12 +6,17 @@ class Shape :
     public GameObject
 {
 public:
-	Shape() = default;
+	explicit Shape(const std::wstring& texturePath = L"") :
+	m_TexturePath{texturePath}
+	{
+		
+	};
     virtual ~Shape() override = default;
 protected:
     void Initialize(const SceneContext&) override;
     MeshRenderComponent* GetMesh() const { return m_pMesh; }
 private:
+    const std::wstring m_TexturePath;
     MeshRenderComponent* m_pMesh{};
 };
 
