310a81ec14bee835b8258970dfe2caa91fe53017
gaslam <44768183+gaslam@users.noreply.github.com>
2024-09-10 20:00:54 +0200

    Add ComponentPool

---
+++ GameEngine/Components/ComponentPool.h
@@ -0,0 +1,70 @@
+#pragma once
+#include "Sets/SparseSet.h"
+
+using Entity = unsigned int;
+
+class IComponentPool
+{
+public:
+	IComponentPool() = default;
+	virtual ~IComponentPool() = default;
+
+	IComponentPool(const IComponentPool&) = delete;
+	IComponentPool(IComponentPool&&) noexcept = delete;
+	IComponentPool& operator=(const IComponentPool&) = delete;
+	IComponentPool& operator=(IComponentPool&&) noexcept = delete;
+};
+
+
+
+template <typename T>
+class ComponentPool final : public IComponentPool
+{
+	SparseSet<Entity> m_Entities{100000,100000};
+	std::vector<T> m_Components{};
+
+public:
+	T& AddComponent(Entity entity)
+	{
+		m_Entities.Insert(entity);
+		return m_Components.emplace_back(T{});
+	}
+
+
+	template <typename... Args>
+	T& AddComponent(Entity entity,Args&&... args)
+	{
+		m_Entities.Insert(entity);
+		return m_Components.emplace_back(T{std::forward<Args>(args)...});
+	}
+
+	bool Contains(Entity entity) const
+	{
+		return m_Entities.Contains(entity);
+	}
+
+	void Remove(Entity entity)
+	{
+		m_Entities.Remove(entity);
+	}
+
+	[[nodiscard]] const T& GetComponent(Entity entity) const
+	{
+		return m_Components[entity];
+	}
+
+	[[nodiscard]] T& GetComponent(Entity entity)
+	{
+		return m_Components[entity];
+	}
+
+	void Clear()
+	{
+		m_Entities.Clear();
+		m_Components.clear();
+	}
+
+
+};
+
+
