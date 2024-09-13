de53c84e9a8127b6465be3c045fb12856b58bce7
gaslam <44768183+gaslam@users.noreply.github.com>
2024-09-13 17:25:03 +0200

    Add GetComponentPool

--- GameEngine/ECS/Registry.h
+++ GameEngine/ECS/Registry.h
@@ -31,6 +31,23 @@ public:
 		return m_ComponentPools.emplace_back(componentName, std::make_unique<ComponentPool<T>>()).second.get();
 	}
 
+	template
+	<typename T>
+	[[nodiscard]] IComponentPool* GetComponentPool(const char* componentName) {
+		const auto found{ std::find_if(m_ComponentPools.begin(), m_ComponentPools.end(), [componentName](const std::pair<const char*, std::unique_ptr<IComponentPool>>& pair) {
+			return strcmp(pair.first, componentName) == 0;
+			}) };
+
+		if (found != m_ComponentPools.end()) {
+			return found->second.get();
+		}
+		return m_ComponentPools.emplace_back(componentName, std::make_unique<ComponentPool<T>>()).second.get();
+	}
+
+	[[nodiscard]] std::vector<std::pair<const char*, std::unique_ptr<IComponentPool>>> GetComponentPools() const {
+		return m_ComponentPools;
+	}
+
 private:
 	std::vector<std::pair<const char*, std::unique_ptr<IComponentPool>>> m_ComponentPools;
 };
